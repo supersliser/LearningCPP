2100232e4965d154bd5c23c393cbdd3bb1333ca9
Thomas Lower <thomaslower9@gmail.com>
2023-10-06 13:26:08 +0100

    basic graphi structure

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,7 +3,198 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
+
+struct Point
+{
+public:
+	int x;
+	int y;
+};
+
+class PointNode {
+public:
+	Point data;
+	PointNode* next;
+};
+
+class PointList {
+private:
+	PointNode* head;
+public:
+	PointList() : head(nullptr) {}
+
+	void Add(Point inputdata)
+	{
+		PointNode* newNode = new PointNode();
+		newNode->data = inputdata;
+		newNode->next = nullptr;
+
+		if (!head) {
+			head = newNode;
+		}
+		else {
+			PointNode* currentNode = head;
+			while (currentNode->next != nullptr) {
+				currentNode = currentNode->next;
+			}
+			currentNode->next = newNode;
+		}
+	}
+
+	void Clear()
+	{
+		while (head != nullptr) {
+			PointNode* temp = head;
+			head = head->next;
+			free(&temp);
+		}
+	}
+
+	Point getItem(int item)
+	{
+		PointNode* currentNode = head;
+		for (int i = 0; i < item; i++)
+		{
+			if (currentNode == nullptr)
+			{
+				// Handle out-of-bounds condition here
+				// You can throw an exception or return a default value as needed.
+			}
+			currentNode = currentNode->next;
+		}
+		return currentNode->data;
+	}
+
+	int Count()
+	{
+		int output = 0;
+		PointNode* currentNode = head;
+		while (currentNode != nullptr)
+		{
+			output++;
+			currentNode = currentNode->next;
+		}
+		return output;
+	}
+
+	~PointList()
+	{
+		Clear(); // Destructor clears the list to prevent memory leaks
+	}
+};
+
+class GraphDrawer {
+public:
+	PointList pointsOnGraph;
+	void writePositiveGradient(int minX, int maxX, int minY = 0, int maxY = 0)
+	{
+		int maxYLength = calculateMaxYLength();
+		int maxXLength = calculateMaxXLength();
+		cout << writeYStarter(maxY, maxYLength);
+		cout << writeX(pointsOnGraph.getItem(0).x, pointsOnGraph.getItem(0).y, maxX, maxYLength, maxXLength);
+		cout << "\n";
+		for (int i = maxY; i >= 1; i--)
+		{
+			Point pointItem = pointsOnGraph.getItem(i);
+			cout << writeYStarter(maxY, maxYLength);
+			cout << writeX(pointItem.x, i, maxX, maxYLength, maxXLength);
+			cout << "\n";
+		}
+		cout << writeRowLines(maxXLength, minX, maxX);
+		cout << writeXBottom(maxXLength, minX, maxX, maxYLength);
+		cout << writeRowLines(maxXLength, minX, maxX);
+		for (int i = -1; i >= minY; i--)
+		{
+			Point pointItem = pointsOnGraph.getItem(i);
+			cout << writeYStarter(maxY, maxYLength);
+			cout << writeX(pointItem.x, pointItem.y, maxX, maxYLength, maxXLength);
+			cout << "\n";
+		}
+	}
+private:
+	int calculateMaxYLength()
+	{
+		int output = 0;
+		for (int i = 0; i < pointsOnGraph.Count(); i++)
+		{
+			if (output < pointsOnGraph.getItem(i).y)
+			{
+				output = pointsOnGraph.getItem(i).y;
+			}
+		}
+		return output;
+	}
+	int calculateMaxXLength()
+	{
+		int output = 0;
+		for (int i = 0; i < pointsOnGraph.Count(); i++)
+		{
+			if (output < pointsOnGraph.getItem(i).x)
+			{
+				output = pointsOnGraph.getItem(i).x;
+			}
+		}
+		return output;
+	}
+	string writeX(int x, int y, int maxX, int maxYLength, int xSize)
+	{
+		string output = writeYStarter(y, maxYLength);
+		for (int i = 0; i < x * xSize; i++)
+		{
+			output += " ";
+		}
+		output += "^";
+		return output;
+	}
+	string writeX(int x1, int x2, int y, int maxX, int maxYLength, int xSize)
+	{
+		string output = writeYStarter(y, maxYLength);
+		{
+			output += " ";
+		}
+		output += "/";
+		for (int i = x1 + 1; i < x2 * xSize; i++)
+		{
+			output += " ";
+		}
+		output += "\\";
+		return output;
+	}
+	string writeYStarter(int y, int maxYLength)
+	{
+		string output = to_string(y);
+		for (int i = 0; i <= maxYLength; i++)
+		{
+			output += " ";
+		}
+		output += "|";
+		return output;
+	}
+	string writeRowLines(int xSize, int minX, int maxX)
+	{
+		string output = "";
+		for (int i = minX; i <= maxX * xSize; i++)
+		{
+			output += "-";
+		}
+		return output;
+	}
+	string writeXBottom(int xSize, int minX, int maxX, int maxYLength)
+	{
+		string output = writeYStarter(0, maxYLength);
+		for (int i = minX; i <= maxX; i++)
+		{
+			output += i;
+			for (int j = to_string(i).length(); j <= xSize; j++)
+			{
+				output += " ";
+			}
+		}
+		return output;
+	}
+};
 
 int main()
 {
@@ -41,46 +232,28 @@ int main()
 	{
 		cout << "\nx = " + to_string(x) + " >>>>>>>> y = " + to_string((m * x) + c);
 	}
+
+	GraphDrawer drawer;
+
+	PointList points;
+
+	for (int y = (m * xstart) + c; y <= (m * xend) + c; y++)
+	{
+		Point item;
+		item.x = (y - c) / m;
+		item.y = y;
+		points.Add(item);
+	}
+
+	drawer.pointsOnGraph = points;
+
+	if (m > 0)
+	{
+		drawer.writePositiveGradient(xstart, xend, (m * xstart) + c, (m * xend) + c);
+	}
 }
 
-class GraphDrawer {
-	
-};
 
-struct Point
-{
-public:
-	int x;
-	int y;
-};
-
-class PointNode {
-public:
-	Point data;
-	PointNode *next;
-};
-
-class PointList {
-private:
-	PointNode *head;
-
-public:
-	void Add(Point inputdata)
-	{
-		PointNode* newNode = new PointNode();
-		newNode->data = inputdata;
-		PointNode* currentNode = head;
-		while (currentNode != NULL)
-		{
-			currentNode = head->next;
-		}
-		currentNode->next = newNode;
-	}
-	void Clear()
-	{
-
-	}
-};
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
